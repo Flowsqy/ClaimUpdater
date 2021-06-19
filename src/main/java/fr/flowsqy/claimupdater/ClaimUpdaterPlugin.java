b08fb7851a2fa2ce82c12034b03ae650bb3378a2
Flowsqy <Flo504.Dev@gmail.com>
2021-06-19 20:51:32 +0200

    Init project

---
+++ src/main/java/fr/flowsqy/claimupdater/ClaimUpdaterPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.claimupdater;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClaimUpdaterPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
